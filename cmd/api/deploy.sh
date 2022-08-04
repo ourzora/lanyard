258dac3836311d3d92610fe90663e880a954aac2
Ryan Smith <r@32k.io>
2022-08-04 15:10:09 -0700

    deploy image directly

---
+++ cmd/api/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -eux
+
+go run github.com/tailscale/mkctr@latest \
+  --base="ghcr.io/tailscale/alpine-base:3.16" \
+  --gopaths="github.com/contextart/al/cmd/api:/usr/local/bin/api" \
+  --tags="latest" \
+  --repos="registry.fly.io/al-prod" \
+  --target=flyio \
+  --push \
+/usr/local/bin/api
+
+flyctl deploy -i registry.fly.io/al-prod:latest
