d030f4d97542dadd2a684a56142f1cf127421b45
Malone Hedges <malonehedges@users.noreply.github.com>
2022-08-10 16:54:18 -0700

    what's a Merkle root

--- www/components/FAQ.tsx
+++ www/components/FAQ.tsx
@@ -12,6 +12,25 @@ export default function FAQ() {
         </Answer>
       </QandA>
       <QandA>
+        <Question>What is a Merkle root?</Question>
+        <Answer>
+          A{' '}
+          <a
+            href="https://en.wikipedia.org/wiki/Merkle_root"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            Merkle root
+          </a>{' '}
+          is the most popular way to handle allow lists for projects using the
+          blockchain. By using a Merkle root, you and your community pay
+          significantly less in gas fees to prove a wallet is allowed to mint a
+          specific project. This is done by proving an address belongs to a tree
+          of hashed values leading to the root.
+        </Answer>
+      </QandA>
+      <QandA>
         <Question>
           What do you mean when you say my allow list will work across web3?
         </Question>
