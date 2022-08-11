86fba56692a2af2c58c46c51223efe2847be7ea1
Malone Hedges <malonehedges@users.noreply.github.com>
2022-08-10 17:25:34 -0700

    collab on top

--- www/components/Layout/index.tsx
+++ www/components/Layout/index.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames'
 import SiteNav from 'components/SiteNav'
 import MetaTags from 'components/MetaTags'
 import { ReactNode } from 'react'
@@ -10,16 +9,10 @@ type Props = {
 
 export default function Layout({ children }: Props) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col items-center overflow-x-hidden">
       <MetaTags description={siteDescription} />
 
-      <div
-        className={classNames(
-          'overflow-x-hidden w-full max-w-screen-xl',
-          'px-3 md:px-8 pb-8',
-          'self-center',
-        )}
-      >
+      <div className="w-full max-w-screen-lg px-3 md:px-8 pb-8">
         <SiteNav />
         <main>{children}</main>
       </div>
--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -3,23 +3,33 @@ import FAQ from 'components/FAQ'
 
 export default function CreatePage() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col mb-24">
       <div className="font-bold text-2xl text-center my-10">
         Create an allow list in seconds that works across web3
       </div>
+
       <CreateRoot />
 
-      <div className="h-px w-full bg-neutral-300 my-24" />
+      <div
+        className="h-px bg-neutral-200 my-16"
+        style={{ marginLeft: '-100vw', marginRight: '-100vw' }}
+      />
+
+      <div className="flex flex-col gap-y-6">
+        <div className="text-xl">Built in collaboration with</div>
+        <div className="flex flex-col sm:flex-row gap-x-10 gap-y-10 items-center">
+          <img src="/zora.png" alt="Zora logo" className="w-24" />
+          <img src="/mintfun.png" alt="mint.fun logo" className="w-24" />
+          <img src="/context.png" alt="Context logo" className="w-24" />
+        </div>
+      </div>
+
+      <div
+        className="h-px bg-neutral-200 my-16"
+        style={{ marginLeft: '-100vw', marginRight: '-100vw' }}
+      />
 
       <FAQ />
-
-      <div className="h-px w-full bg-neutral-300 my-24" />
-
-      <div className="flex flex-col md:flex-row gap-x-20 gap-y-10 items-center justify-center mb-16">
-        <img src="/zora.png" alt="Zora logo" className="w-52" />
-        <img src="/mintfun.png" alt="mint.fun logo" className="w-52" />
-        <img src="/context.png" alt="Context logo" className="w-52" />
-      </div>
     </div>
   )
 }
