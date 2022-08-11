8a8c392bb2b222944b60b6ecdfaa51ef0952905b
Malone Hedges <malonehedges@users.noreply.github.com>
2022-08-10 18:05:18 -0700

    fonts fix

--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -6,8 +6,32 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Chivo:wght@400;700&display=swap"
-          rel="stylesheet"
+          rel="preload"
+          href="/fonts/chivo400ext.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/chivo400.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/chivo700ext.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/chivo700.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
         />
       </Head>
       <body className={bodyStyles}>
