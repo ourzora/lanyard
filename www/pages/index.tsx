0c1ff618713b57f90ce364791f879f64ab0eeabf
Malone Hedges <malonehedges@users.noreply.github.com>
2022-08-11 11:09:21 -0700

    update collab

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -10,26 +10,40 @@ export default function CreatePage() {
 
       <CreateRoot />
 
-      <div
-        className="h-px bg-neutral-200 my-16"
-        style={{ marginLeft: '-100vw', marginRight: '-100vw' }}
-      />
-
-      <div className="flex flex-col gap-y-6">
-        <div className="text-xl">Built in collaboration with</div>
-        <div className="flex flex-col sm:flex-row gap-x-10 gap-y-10 items-center">
-          <img src="/zora.png" alt="Zora logo" className="w-24" />
-          <img src="/mintfun.png" alt="mint.fun logo" className="w-24" />
-          <img src="/context.png" alt="Context logo" className="w-24" />
+      <div className="flex gap-3 sm:gap-4 text-base mt-10 items-center">
+        <img
+          src="/collablogos.png"
+          alt="partner logos"
+          className="w-[64px] sm:w-[84px]"
+        />
+        <div>
+          An open source project from{' '}
+          <PartnerLink href="https://context.app">Context</PartnerLink>,{' '}
+          <PartnerLink href="https://zora.co">Zora</PartnerLink>, and{' '}
+          <PartnerLink href="https://mint.fun">mint.fun</PartnerLink>
         </div>
       </div>
 
-      <div
-        className="h-px bg-neutral-200 my-16"
-        style={{ marginLeft: '-100vw', marginRight: '-100vw' }}
-      />
+      <div className="h-px bg-neutral-200 my-10 sm:my-16 w-full" />
 
       <FAQ />
     </div>
   )
 }
+
+const PartnerLink = ({
+  href,
+  children,
+}: {
+  href: string
+  children: React.ReactNode
+}) => (
+  <a
+    href={href}
+    className="font-bold hover:underline"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {children}
+  </a>
+)
