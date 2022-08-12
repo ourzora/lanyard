e4efc5fa084c9984baf6761ea148ba9eb62b0bd0
Malone Hedges <malonehedges@users.noreply.github.com>
2022-08-11 19:35:28 -0700

    meta

--- www/components/Layout/Head.tsx
+++ www/components/Layout/Head.tsx
@@ -8,6 +8,26 @@ function Head() {
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1"
       />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#6dfa8c" />
+      <meta name="theme-color" content="#6dfa8c" />
     </NextHead>
   )
 }
--- www/components/Layout/index.tsx
+++ www/components/Layout/index.tsx
@@ -1,7 +1,8 @@
+import { ReactNode } from 'react'
 import SiteNav from 'components/SiteNav'
 import MetaTags from 'components/MetaTags'
-import { ReactNode } from 'react'
 import { siteDescription } from 'utils/constants'
+import Head from './Head'
 
 type Props = {
   children: ReactNode
@@ -10,7 +11,8 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <div className="flex flex-col items-center overflow-x-hidden pb-20">
-      <MetaTags description={siteDescription} />
+      <MetaTags description={siteDescription} imageUrl="/meta-image.png" />
+      <Head />
 
       <div className="w-full max-w-screen-lg px-3 sm:px-8 pb-8">
         <SiteNav />
