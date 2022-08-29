b23545d4feb3971c8918133eeae996df97f3938c
Malone Hedges <malonehedges@users.noreply.github.com>
2022-08-29 16:19:39 -0400

    www: update meta images (#57)

--- www/components/Layout/Head.tsx
+++ www/components/Layout/Head.tsx
@@ -11,21 +11,22 @@ function Head() {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-touch-icon.png"
+        href="/apple-touch-icon.png?v=2"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="/favicon-32x32.png?v=2"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="/favicon-16x16.png?v=2"
       />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="manifest" href="/site.webmanifest?v=2" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#6dfa8c" />
       <meta name="msapplication-TileColor" content="#6dfa8c" />
       <meta name="theme-color" content="#6dfa8c" />
     </NextHead>
