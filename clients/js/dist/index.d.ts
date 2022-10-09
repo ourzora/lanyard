410da86d66313926a1501b05b72816f733d78b6e
luke miles <luke@context.app>
2022-10-09 10:53:48 -0700

    remove dist

--- clients/js/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { CreateTreeRequest, CreateTreeResponse, GetProofResponse, GetRootsResponse, GetTreeResponse } from "./types";
-export declare const CreateTree: (req: CreateTreeRequest) => Promise<CreateTreeResponse>;
-export declare const GetTree: (merkleRoot: string) => Promise<GetTreeResponse>;
-export declare const GetProof: (root: string, unhashedLeaf: string) => Promise<GetProofResponse>;
-export declare const GetRoots: (proof: string) => Promise<GetRootsResponse>;
-export * from "./types";
